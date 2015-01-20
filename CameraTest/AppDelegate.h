aa9ccc2403420d8af978c68ccfd289095b617099
Joel Klabo <jklabo@yammer-inc.com>
2015-01-20 13:34:39 -0800

    Initial Commit

---
+++ CameraTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraTest
+//
+//  Created by Joel Klabo on 1/20/15.
+//  Copyright (c) 2015 Joel Klabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraTest
+//
+//  Created by Joel Klabo on 1/20/15.
+//  Copyright (c) 2015 Joel Klabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
