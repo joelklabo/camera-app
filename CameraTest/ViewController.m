4f0077ee54ec29cab749436a2bf5f3886eabf73c
Joel Klabo <jklabo@yammer-inc.com>
2015-01-26 11:36:35 -0800

    Take or choose phote, surface most recent photo in library

--- CameraTest/ViewController.m
+++ CameraTest/ViewController.m
@@ -7,21 +7,91 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <Photos/Photos.h>
 
-@interface ViewController ()
+@interface ViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *chosenImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *recentImageView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    PHFetchOptions *fetchOptions = [[PHFetchOptions alloc] init];
+    fetchOptions.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:YES]];
+    PHFetchResult *fetchResult = [PHAsset fetchAssetsWithMediaType:PHAssetMediaTypeImage options:fetchOptions];
+    PHAsset *lastAsset = [fetchResult lastObject];
+    [[PHImageManager defaultManager] requestImageForAsset:lastAsset
+                                               targetSize:self.recentImageView.bounds.size
+                                              contentMode:PHImageContentModeAspectFill
+                                                  options:PHImageRequestOptionsVersionCurrent
+                                            resultHandler:^(UIImage *result, NSDictionary *info) {
+                                                
+                                                dispatch_async(dispatch_get_main_queue(), ^{
+                                                    
+                                                    self.recentImageView.image = result;
+                                                    
+                                                });
+                                            }];
+    
+
+    UITapGestureRecognizer *singleFingerTap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                            action:@selector(recentImageViewTap:)];
+    self.recentImageView.userInteractionEnabled = YES;
+    [self.recentImageView addGestureRecognizer:singleFingerTap];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)takePictureTapped:(id)sender
+{
+    NSLog(@"takePictureTapped");
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (IBAction)choosePictureTapped:(id)sender
+{
+    NSLog(@"choosePictureTapped");
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.chosenImageView.image = chosenImage;
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)recentImageViewTap:(UITapGestureRecognizer *)recognizer
+{
+    if (self.recentImageView.image) {
+        self.chosenImageView.image = self.recentImageView.image;
+    } else {
+        [self choosePictureTapped:nil];
+    }
 }
 
 @end
